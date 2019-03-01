62138253863c30bae026546edd117fd789dc28ea
davidtron <david@spiraltechnology.co.uk>
2019-03-01 06:10:47 +0000

    Attempting to wire in audit app

---
+++ static/audits/precache-manifest.5abc2e50c877a8f067466c71df5ca8b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e8462ff34e2e56d68c2",
+    "url": "/audits/static/css/main.ac461124.chunk.css"
+  },
+  {
+    "revision": "6e8462ff34e2e56d68c2",
+    "url": "/audits/static/js/main.6e8462ff.chunk.js"
+  },
+  {
+    "revision": "9d83ec0cf22474107c5d",
+    "url": "/audits/static/css/1.b8a09fd3.chunk.css"
+  },
+  {
+    "revision": "9d83ec0cf22474107c5d",
+    "url": "/audits/static/js/1.9d83ec0c.chunk.js"
+  },
+  {
+    "revision": "8dbc246fddd19fa74126",
+    "url": "/audits/static/js/runtime~main.8dbc246f.js"
+  },
+  {
+    "revision": "1796459485849bd63191670def10e717",
+    "url": "/audits/index.html"
+  }
+];
